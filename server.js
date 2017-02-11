8ee37d5991f0f7e23043e8b525e70a88a7cfb023
miningape <miningape@gmail.com>
2017-02-11 21:35:27 +0100

    new commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+var visitor = 0;
+var port = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res, next) {
+	res.sendFile(__dirname + "/webpages/index.html");
+	console.log("Visitor: " + ++visitor)
+});
+
+app.get('/code', function (req, res, next) {
+	res.sendFile(__dirname + "/webpages/code.html");
+});
+
+app.get('/about', function (req, res, next) {
+	res.sendFile(__dirname + "/webpages/about.html");
+});
+
+app.get('/contact', function (req, res, next) {
+	res.sendFile(__dirname + "/webpages/contact.html");
+});
+
+app.listen(port, function (){
+	console.log("Listening on localhost:80");
+});
