c77de96f8505a549bcd14f0e3524fe48fb5a96dc
miningape <miningape@gmail.com>
2017-02-12 18:18:05 +0100

    Login system added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 var visitor = 0;
+var loggedIn = false;
 var port = process.env.PORT || 8080;
 
 app.use(express.static('public'));
@@ -9,6 +10,7 @@ app.use(express.static('public'));
 app.get('/', function (req, res, next) {
 	res.sendFile(__dirname + "/webpages/index.html");
 	console.log("Visitor: " + ++visitor, req.connection.remoteAddress);
+
 });
 
 app.get('/code', function (req, res, next) {
@@ -21,6 +23,26 @@ app.get('/about', function (req, res, next) {
 
 app.get('/contact', function (req, res, next) {
 	res.sendFile(__dirname + "/webpages/contact.html");
+});
+
+app.get('/login', function (req, res, next) {
+	response = {
+		username: req.query.username,
+		password: req.query.password
+	};
+
+	if (response.username == 'kyle' && response.password == 'hello') {
+		loggedIn = true;
+		res.redirect('/admin-page');
+	}
+});
+
+app.get('/admin-page', function (res, req, next) {
+	if (loggedIn) {
+		res.sendFile(__dirname + "/webpages/admin-page");
+	} else {
+		res.sendFile(__dirname + "/webpages/login-page")
+	}
 });
 
 app.listen(port, function (){
