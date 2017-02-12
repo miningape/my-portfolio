bfe47dc8a3d397e8564856eb278246068341ddc1
miningape <miningape@gmail.com>
2017-02-12 20:29:22 +0100

    Session based authentication

--- server.js
+++ server.js
@@ -32,18 +32,28 @@ app.get('/login', function (req, res, next) {
 	};
 
 	if (response.username == 'kyle' && response.password == 'hello') {
+		req.session.user_id = response;
 		loggedIn = true;
 		res.redirect('/admin-page');
 	}
 });
 
-app.get('/admin-page', function (req, res, next) {
+app.get('/admin-page', auth(), function (req, res, next) {
 	if (loggedIn) {
 		res.sendFile(__dirname + "/webpages/admin-page.html");
 	} else {
 		res.sendFile(__dirname + "/webpages/login-page.html");
 	}
 });
+
+function auth (req, res, next) {
+	if (!req.session.user_id) {
+		loggedIn = false;
+		next();
+	} else {
+		nexx();
+	}
+}
 
 app.listen(port, function (){
 	console.log("Listening on localhost:80");
